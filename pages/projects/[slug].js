bacdcc6239de105437167dbc75ea5f39b3f36ea8
Ismail Rhilal <ismailrhilal@gmail.com>
2022-05-14 20:13:21 +0100

    minor

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -34,8 +34,11 @@ function Project({ project }) {
 						/>
 					</div>
 				))}
-				<Link href={project.demo} passHref>
-					<a className="bg-white text-primary-2 text-base m-4 px-4 py-2 border-2 border-white hover:bg-transparent hover:text-white  hover:fill-white fill-primary-2">
+				<Link passHref>
+					<a
+						target="_blank"
+						className="bg-white text-primary-2 text-base m-4 px-4 py-2 border-2 border-white hover:bg-transparent hover:text-white  hover:fill-white fill-primary-2"
+					>
 						<div className="flex flex-row gap-2">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
